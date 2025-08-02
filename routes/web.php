5fae08cb2e78ba47536b2402755a15fe31994709
omotola-29 <motolaa021@gmail.com>
2025-08-02 17:15:44 +0100

    Special Mission part 1 and 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
+
 
 
 
@@ -20,5 +23,13 @@ Route::view('/contact', 'contact');
 
 
 Route::resource('jobs', JobController::class);
+
+//auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
 
 
