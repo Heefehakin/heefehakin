eebf38db5d81b9e613960cbf5450a88c6290e773
omotola-29 <motolaa021@gmail.com>
2025-08-04 05:10:51 +0100

    day 24,Email Delivery

---
+++ resources/views/mail/job-posted.blade.php
@@ -0,0 +1,6 @@
+<h2>{{ $job->title }}</h2>
+
+<p>Congrats! Your job is now live on our website</p>
+
+<p><a href="{{ url('/jobs', $job->id) }}">View Your Job Listing</a></p>
+   {{-- or "{{ url('/jobs/' . $job->id) }}" jef main example--}}
